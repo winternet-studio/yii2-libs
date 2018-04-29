4390be72b089a35fd88adf9c2cf4239199436549
winternet-studio <github654@allanid.com>
2018-04-29 21:53:20 +0200

    Added class MessageFormatterFallback

---
+++ MessageFormatterFallback.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Force using message formatter fallback method
+ *
+ * Skip using PHP intl extension even if it is available, in case it is of an ICU version that is too old and server cannot be upgraded.
+ *
+ * Based on ideas from https://stackoverflow.com/questions/48672978/yii2-how-to-force-using-fallback-messageformatter-method
+ *
+ * Usage example:
+ * ```
+ * $fbFormatter = new \app\components\MessageFormatterCustom();
+ * echo $fbFormatter->format('{number,plural,=0{# available} =1{# available} other{# available}}', ['number' => 45], Yii::$app->language);
+ * ```
+ */
+
+namespace app\components;
+
+class MessageFormatterFallback extends \yii\i18n\MessageFormatter {
+    private $_errorCode = 0;
+    private $_errorMessage = '';
+
+	public function format($pattern, $params, $language) {
+		$this->_errorCode = 0;
+		$this->_errorMessage = '';
+
+		if ($params === []) {
+			return $pattern;
+		}
+
+		return $this->fallbackFormat($pattern, $params, $language);
+	}
+}
