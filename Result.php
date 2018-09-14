a7e8610208adba3ba05700fa8fe1a0a19ab0bb37
winternet-studio <info@winternet.no>
2018-09-14 13:53:40 +0200

    Added Result class for handling errors and notices in methods/functions

---
+++ Result.php
@@ -0,0 +1,58 @@
+<?php
+namespace winternet\yii2;
+
+use Yii;
+use yii\base\Component;
+
+/**
+ * This Yii component can be used to collect errors and notices (result messages) in methods/functions
+ */
+class Result extends Component {
+
+	public $status = 'ok';
+
+	protected $errorMessages = [];
+
+	protected $resultMessages = [];
+
+	public function addError($message) {
+		$this->errorMessages[] = $message;
+	}
+
+	public function addNotice($message) {
+		$this->resultMessages[] = $message;
+	}
+
+	public function getErrors() {
+		return $this->errorMessages;
+	}
+
+	public function getNotices() {
+		return $this->resultMessages;
+	}
+
+	public function resetErrors() {
+		$this->errorMessages = [];
+	}
+
+	public function resetNotices() {
+		$this->resultMessages = [];
+	}
+
+	public function output() {  //if I change the structure one day a new method could maybe be called toArray()...
+		if (count($this->errorMessages) == 0) {
+			return array(
+				'status' => $this->status,
+				'result_msg' => $this->resultMessages,
+				'err_msg' => array(),
+			);
+		} else {
+			$this->status = 'error';
+			return array(
+				'status' => $this->status,
+				'result_msg' => array(),
+				'err_msg' => $this->errorMessages,
+			);
+		}
+	}
+}
