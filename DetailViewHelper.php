8b5b2d35fd33613e98bb4456529f1173e11513c6
winternet-studio <info@winternet.no>
2018-12-06 18:32:25 +0100

    Added methods

--- DetailViewHelper.php
+++ DetailViewHelper.php
@@ -5,11 +5,11 @@ use Yii;
 use yii\base\Component;
 
 class DetailViewHelper extends Component {
-	// public static function fromAllowedValues($model, $attribute) {
-	// 	return
-	// 		[
-	// 			'attribute' => $attribute
-	// 			'value' => $model::allowedValues('ev_is_active')[ $model->$attribute ],
-	// 		];
-	// }
+	public static function fromAllowedValues($model, $attribute) {
+		return
+			[
+				'attribute' => $attribute,
+				'value' => $model::allowedValues($attribute)[ $model->$attribute ],
+			];
+	}
 }
--- GridViewHelper.php
+++ GridViewHelper.php
@@ -4,11 +4,28 @@ namespace winternet\yii2;
 use Yii;
 use yii\base\Component;
 use yii\helpers\Html;
-use yii\helpers\Json;
-use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
 
 class GridViewHelper extends Component {
-	// public static function firstMethod() {
+	public static function filterFromAllowedValues($searchModel, $attribute, $options = []) {
+		return
+			[
+				'attribute' => $attribute,
+				'filter' => Html::activeDropDownList($searchModel, $attribute, $searchModel::allowedValues($attribute), ['prompt' => (array_key_exists('prompt', $options) ? $options['prompt'] : ''), 'class' => 'form-control'. ($options['class'] ? ' '. $options['class'] : ''), 'style' => 'width: '. ($options['width'] ? $options['width'] : '85px') ]),
+				'value' => function($model) {
+					return $model::allowedValues($attribute)[ $model->$attribute ],
+				},
+			];
+	}
 
-	// }
+	/**
+	 * @param $dataArray : Example: `\app\models\Customer::find()->orderBy('cust_name')->all()`
+	 */
+	public static function filterFromArray($searchModel, $attribute, $dataArray, $dataArrayKey, $dataArrayValue $options = []) {
+		return
+			[
+				'attribute' => ($options['relatedAttribute'] ? $options['relatedAttribute'] : $attribute),
+				'filter' => Html::activeDropDownList($searchModel, $attribute, ArrayHelper::map($dataArray, $dataArrayKey, $dataArrayValue), ['prompt' => (array_key_exists('prompt', $options) ? $options['prompt'] : ''), 'class' => 'form-control'. ($options['class'] ? ' '. $options['class'] : '') ]),
+			];
+	}
 }
