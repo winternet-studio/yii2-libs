bec47326b76a1d6821a36c30aaa885f3afafa28e
winternet-studio <info@winternet.no>
2018-12-06 17:08:55 +0100

    Added DetailViewHelper and GridViewHelper (empty though)

---
+++ DetailViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+namespace winternet\yii2;
+
+use Yii;
+use yii\base\Component;
+
+class DetailViewHelper extends Component {
+	// public static function fromAllowedValues($model, $attribute) {
+	// 	return
+	// 		[
+	// 			'attribute' => $attribute
+	// 			'value' => $model::allowedValues('ev_is_active')[ $model->$attribute ],
+	// 		];
+	// }
+}
---
+++ GridViewHelper.php
@@ -0,0 +1,14 @@
+<?php
+namespace winternet\yii2;
+
+use Yii;
+use yii\base\Component;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\helpers\Url;
+
+class GridViewHelper extends Component {
+	// public static function firstMethod() {
+
+	// }
+}
