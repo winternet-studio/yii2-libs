6aa7afe5c593c90ebc2867cfd8286e747f3843a0
winternet-studio <info@winternet.no>
2019-10-02 11:19:18 +0200

    Bugfix

--- test/CustomActiveFixture.php
+++ test/CustomActiveFixture.php
@@ -1,6 +1,7 @@
 <?php
 namespace winternet\yii2\test;
 
+use yii\base\InvalidConfigException;
 use yii\helpers\ArrayHelper;
 
 /**
@@ -10,13 +11,19 @@ class CustomActiveFixture extends \yii\test\ActiveFixture {
 	/**
 	 * {@inheritdoc}
 	 *
-	 * This custom version will not delete all records in table when unloading fixtures but only the fixture data that was inserted
+	 * This custom version will not delete all records in table when unloading fixtures but only the fixture data that was inserted.
+	 * Currently only usable when a primary key exists and consists of a exactly one column.
 	 */
 	protected function resetTable() {
 		$table = $this->getTableSchema();
-		$IDs = ArrayHelper::getColumn($this->data, 'discount_codeID');
+
+		if (count($table->primaryKey) !== 1) {
+            throw new InvalidConfigException(self::class .' currently only supports tables with a primary key consisting of a exactly one column.');
+		}
+
+		$IDs = ArrayHelper::getColumn($this->data, $table->primaryKey[0]);
 		if (!empty($IDs)) {
-			$this->db->createCommand()->delete($table->fullName, ['in', 'discount_codeID', $IDs])->execute();
+			$this->db->createCommand()->delete($table->fullName, ['in', $table->primaryKey[0], $IDs])->execute();
 			if ($table->sequenceName !== null) {
 				$this->db->createCommand()->executeResetSequence($table->fullName, 1);
 			}
