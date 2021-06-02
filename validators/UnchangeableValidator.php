d5924655978388edfba8d1cea389dc00275df544
Allan Jensen <info@winternet.no>
2021-06-02 11:27:18 +0200

    Added UnchangeableValidator

---
+++ validators/UnchangeableValidator.php
@@ -0,0 +1,19 @@
+<?php
+namespace winternet\yii2\validators;
+
+use Yii;
+use yii\validators\Validator;
+
+class UnchangeableValidator extends Validator {
+
+	/**
+	 * @var boolean : Value passed to the `$identical` argument for `isAttributeChanged()`
+	 */
+	public $identical = true;
+
+	public function validateAttribute($model, $attribute) {
+		if ( ! $model->isNewRecord && $model->isAttributeChanged($attribute, $this->identical)) {
+			$this->addError($model, $attribute, ($this->message ? $this->message : Yii::t('yii', '{attribute} cannot be changed.')));
+		}
+	}
+}
