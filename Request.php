0a66152c0e8593953b5ddd14ccd01f8deb942289
Allan Jensen <info@winternet.no>
2022-04-06 19:58:59 +0200

    Added Request class for handling query parameters in controller actions

---
+++ Request.php
@@ -0,0 +1,38 @@
+<?php
+namespace winternet\yii2;
+
+use Yii;
+use yii\base\Component;
+
+class Request extends Component {
+
+	/**
+	 * Handle action parameters in controller, incl. numeric value conversion
+	 *
+	 * The purpose is to solve the issue when the parameter has a dash or other special character in it prohibiting the normal parameters in the action method.
+	 */
+	public static function queryParam($name, $default = null) {
+		$value = \Yii::$app->request->get($name);
+		if (empty($value) && !is_numeric($value)) {
+			if ($default === null) {
+				throw new \yii\web\BadRequestHttpException('Missing required parameter: '. $name);
+			} else {
+				return $default;
+			}
+		}
+
+		// Handle type conversion for numeric values as well
+		if (is_numeric($value)) {
+			if (is_int($default) || is_float($default)) {
+				if (floor($value) != (float) $value) {
+					$value = (float) $value;
+				} else {
+					$value = (integer) $value;
+				}
+			}
+		}
+
+		return $value;
+	}
+
+}
